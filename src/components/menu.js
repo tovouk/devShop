464d382f770928ee6af1f1ff41d2dca4f07fa288
Jose Hinojo <burningremedy@gmail.com>
2018-04-13 23:53:10 +0000

    Add surrounding div to menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Menu = () =>(
-    <ul style={{
-        width:'100vw',
-        height:'auto',
-        display:'inline',
-        background:'#666'
-        
-    }}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/page-2/">Page 2</Link></li>
-    </ul>
+    <div>
+        <ul style={{
+            width:'100vw',
+            height:'auto',
+            display:'inline',
+            background:'#666'
+            
+        }}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/page-2/">Page 2</Link></li>
+        </ul>
+    </div>
 )
     
 export default Menu
