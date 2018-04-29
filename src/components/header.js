3ae67ed8204b43454452beec64cca665c03fad87
Jose Hinojo <burningremedy@gmail.com>
2018-04-29 01:44:52 +0000

    header class responsive

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,7 @@ import Link from 'gatsby-link'
 import Menu from './menu'
 
 const Header = ({ siteTitle }) => (
-  <div style={{
-        margin: '0 auto',
-        padding: '1.45rem 1.0875rem',
-        background: 'rebeccapurple',
-        display:"flex",
-        justifyContent:"space-between",
-        height:"100vh",
-        background:"url(https://images.unsplash.com/photo-1510843572979-e4b9e790fdd7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=238526856465b28e7372a88c82ee05d5&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb)",
-        backgroundRepeat:"no-repeat",
-        backgroundSize:"cover"
-  }}>
+  <div className="header">
     <div>
       <h1 style={{
       }}>
